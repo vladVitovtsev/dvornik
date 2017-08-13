715c7f4bfef6875f4ffa6ae06e2b7411250ce023
v.vitovtsev <vlad.vitovtsev@mail.ru>
2017-08-13 16:20:06 +0700

    fix migration

--- database/migrations/2017_08_13_071900_create_orders_table.php
+++ database/migrations/2017_08_13_071900_create_orders_table.php
@@ -13,14 +13,14 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->string('products');
-            $
-            $table->timestamps();
-        });
+//        Schema::create('orders', function (Blueprint $table) {
+//            $table->increments('id');
+//            $table->integer('user_id')->unsigned();
+//            $table->foreign('user_id')->references('id')->on('users');
+//            $table->string('products');
+//
+//            $table->timestamps();
+//        });
     }
 
     /**
