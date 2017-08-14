2644842d4db2a821834ae420dc076cce17ffc863
v.vitovtsev <vlad.vitovtsev@mail.ru>
2017-08-15 01:35:24 +0700

    test git pull revert

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -16,7 +16,6 @@
             </div><!-- /.box tools -->
         </div><!-- /.box-header -->
         <div class="box-body">
-            123
         </div><!-- /.box-body -->
     </div><!--box box-success-->
 
